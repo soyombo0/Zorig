caa1384dd617d78b5f365bd09d82ab5aedecadef
soyombo <soyombo0@naver.com>
2023-01-30 18:54:32 +0300

    Initial Commit

---
+++ Zorig/ZorigApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZorigApp.swift
+//  Zorig
+//
+//  Created by Soyombo Mantaagiin on 30.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ZorigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
