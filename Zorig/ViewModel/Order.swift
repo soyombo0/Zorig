9cff1a6fbb33a94efff3e26d12c144a5305af0d9
soyombo <soyombo0@naver.com>
2023-01-31 21:46:20 +0300

    All files were segregated

---
+++ Zorig/ViewModel/Order.swift
@@ -0,0 +1,32 @@
+//
+//  Order.swift
+//  Zorig
+//
+//  Created by Soyombo Mantaagiin on 31.01.2023.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    
+    @Published var items = [MenuItem]()
+
+    var total: Int {
+        if items.count > 0 {
+            return items.reduce(0) { $0 + $1.price }
+        } else {
+            return 0
+        }
+    }
+
+    func add(item: MenuItem) {
+        items.append(item)
+    }
+
+    func remove(item: MenuItem) {
+        if let index = items.firstIndex(of: item) {
+            items.remove(at: index)
+        }
+    }
+}
+
--- Zorig/ViewModel/ZorigApp.swift
+++ Zorig/ViewModel/ZorigApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct ZorigApp: App {
+    
+    @StateObject var order = Order()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(order)
         }
     }
 }
